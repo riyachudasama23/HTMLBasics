5050498d62f0a528530f377a14e4e06b25186189
Riya <riyachudasama1123@gmail.com>
2025-09-11 18:24:19 +0530

    added position file

--- files/10.bmi.js
+++ files/10.bmi.js
@@ -1,12 +1,34 @@
-let mass1 = 78;
-let height1 = 1.69;
-let mass2 = 92;
-let height2 = 1.95;
+// let mass1 = 78;
+// let height1 = 1.69;
+// let mass2 = 92;
+// let height2 = 1.95;
 
-console.log("hii");
-let bmi1 = mass1 / (height1 * height1);
-let bmi2 = mass2 / (height2 * height2);
+// console.log("hii");
+// let bmi1 = mass1 / (height1 * height1);
+// let bmi2 = mass2 / (height2 * height2);
 
-let markHigherBMI = bmi1 > bmi2;
-console.log("BMI of Mark: ", bmi1);
-console.log("BMI of John: ", bmi2);
+// let markHigherBMI = bmi1 > bmi2;
+// console.log("BMI of Mark: ", bmi1);
+// console.log("BMI of John: ", bmi2);
+
+function calcBMI(mass, height){
+  return mass / (height * height);
+}
+
+function compareBMI(markMass, markHeight, johnMass, johnHeight){
+  const markBMI = calcBMI(markMass, markHeight);
+  const johnBMI = calcBMI(johnMass, johnHeight);
+  const markHigherBMI = markBMI > johnBMI;
+
+  console.log(`Mark's BMI: ${markBMI.toFixed(2)}`);
+  console.log(`John's BMI: ${johnBMI.toFixed(2)}`);
+  console.log(`Does Mark have a higher BMI? ${markHigherBMI}`);
+  console.log("--------------------------------------------------\n");
+}
+
+//Test Data
+console.log("Data 1:");
+compareBMI(78, 1.69, 92, 1.95);
+
+console.log("Data 2:");
+compareBMI(95, 1.88, 85, 1.76);
