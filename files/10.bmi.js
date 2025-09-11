82852eee88d562e97166c71051a9bd75e89d48c4
Riya <riyachudasama1123@gmail.com>
2025-09-11 14:26:17 +0530

    added registration-form, border-styling and layered-div

---
+++ files/10.bmi.js
@@ -0,0 +1,12 @@
+let mass1 = 78;
+let height1 = 1.69;
+let mass2 = 92;
+let height2 = 1.95;
+
+console.log("hii");
+let bmi1 = mass1 / (height1 * height1);
+let bmi2 = mass2 / (height2 * height2);
+
+let markHigherBMI = bmi1 > bmi2;
+console.log("BMI of Mark: ", bmi1);
+console.log("BMI of John: ", bmi2);
